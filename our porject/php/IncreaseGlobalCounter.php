06768dad09af207b41bcba1a3e373404b2cf5a09
asad <asadposti@gmail.com>
2020-11-18 00:05:23 +0100

    labdetema6

--- our porject/php/IncreaseGlobalCounter.php
+++ our porject/php/IncreaseGlobalCounter.php
@@ -1,12 +1,9 @@
 <?php
 
 $email = $_GET["email"];
- function alertredirect($mensaje, $email){
-   // echo "<script type='text/javascript'>alert('$mensaje'); window.location.href = 'Layout.php?email=$email'; </script>";
-  }
+
 
 $xml = simplexml_load_file("../xml/Counter.xml");
-// Create a child in the first topic node
 $encontrado = false ;
 foreach ($xml->user as $user){
     if ($email== ($user->p)){
@@ -21,9 +18,7 @@ $child-> addChild('p',$_GET["email"]);
 }
 
 
-// Add the text attribute
 $xml->asXML('../xml/Counter.xml');
-//alertredirect("Bienvenido " . $email . "!", $email);
 $username=$_GET["email"];
 echo("<script>window.location.href='Layout.php?email=$username&cont=1';</script>");
 //header('Location:Layout.php?email=".$username."&cont=1');

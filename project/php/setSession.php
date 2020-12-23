ce9362327f20f36f0d0886a85a837dedf7e35ac3
Asad <asadamrvi@gmail.com>
2020-12-23 23:52:13 +0100

    as

--- project/php/setSession.php
+++ project/php/setSession.php
@@ -2,7 +2,7 @@
     session_start();
     if(isset($_POST['username'])){
         $_SESSION['username']=$_POST['username'];
-        //$_SESSION['avatar']=$_GET['avatar'];
+        $_SESSION['avatar']=$_POST['avatar'];
         //echo("<script>alert('Bienvenido".$_POST['username']."'); location.href='IncreaseGlobalCounter.php';</script>");
     }
 ?>
